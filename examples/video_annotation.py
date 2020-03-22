6655657181e8b14f31d5e28cc038f4e0d9281401
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-03-22 17:25:08 +0100

    add loader and keybindings

--- examples/video_annotation.py
+++ examples/video_annotation.py
@@ -2,5 +2,6 @@ from skimage import data
 
 from pointannotator import point_annotator
 
-
-point_annotator(data.astronaut(), labels=['Spaceship', 'Astronaut'])
+im_path = '/Users/yamauc0000/Documents/DeepLabCut/examples/openfield-Pranav-2018-10-30/labeled-data/m4s1/*.png'
+output = '/Users/yamauc0000/Documents/PointAnnotator/examples/anno.csv'
+point_annotator(im_path, labels=['ear_l', 'ear_r', 'tail'], output_path=output)
--- pointannotator/gui.py
+++ pointannotator/gui.py
@@ -1,23 +1,41 @@
 from enum import Enum
-from itertools import cycle
+import glob
 from typing import List
 
+from dask_image.imread import imread
 import napari
 import numpy as np
 from magicgui import magicgui
+import pandas as pd
+
+COLOR_CYCLE = [
+        '#1f77b4',
+        '#ff7f0e',
+        '#2ca02c',
+        '#d62728',
+        '#9467bd',
+        '#8c564b',
+        '#e377c2',
+        '#7f7f7f',
+        '#bcbd22',
+        '#17becf'
+]
 
 
-def point_annotator(im: np.ndarray, labels: List[str]):
+def point_annotator(im_path: str, labels: List[str], output_path: str='anno.csv', scorer: str='user'):
+    stack = imread(im_path)
     with napari.gui_qt():
-        viewer = napari.view_image(im)
+        viewer = napari.view_image(stack, contrast_limits=[0, 256], is_pyramid=False)
+        properties = {'label': np.empty(0)}
         default_properties = {'label': np.array(labels)}
         points_layer = viewer.add_points(
-            properties={'label': np.empty(0)},
+            properties=properties,
             default_properties=default_properties,
+            edge_color='label',
+            edge_color_cycle=COLOR_CYCLE,
             symbol='o',
             face_color='transparent',
             edge_width=8,
-            edge_color='label',
             size=12,
         )
         points_layer.edge_color_mode = 'cycle'
@@ -40,6 +58,66 @@ def point_annotator(im: np.ndarray, labels: List[str]):
             label = points_layer.current_properties['label'][0]
             index = label_menu.label_widget.findText(label)
             label_menu.label_widget.setCurrentIndex(index)
+
+        @viewer.bind_key('.')
+        def next_label(event=None):
+            current_properties = points_layer.current_properties
+            current_label = current_properties['label'][0]
+            ind = list(labels).index(current_label)
+            new_ind = (ind + 1) % len(labels)
+            new_label = labels[new_ind]
+            current_properties['label'] = np.array([new_label])
+            points_layer.current_properties = current_properties
+
+
+        def next_on_click(layer, event):
+            if layer.mode == 'add':
+                next_label()
+
+        points_layer.mouse_drag_callbacks.append(next_on_click)
+
+
+        @viewer.bind_key(',')
+        def prev_label(event):
+            current_properties = points_layer.current_properties
+            current_label = current_properties['label'][0]
+            ind = list(labels).index(current_label)
+            n_labels = len(labels)
+            new_ind = ((ind - 1) + n_labels) % n_labels
+            new_label = labels[new_ind]
+            current_properties['label'] = np.array([new_label])
+            points_layer.current_properties = current_properties
+
+
+        # bind the saving function to
+        @viewer.bind_key('Control-S')
+        def save_points(event):
+
+            # get the frame indices
+            frame_indices = np.unique(points_layer.data[:, 0]).astype(np.int)
+
+            # get the filenames
+            all_files = np.asarray(glob.glob(im_path))
+            file_names = all_files[frame_indices]
+
+            # create and write dataframe
+            header = pd.MultiIndex.from_product(
+                [[scorer], labels, ['x', 'y']],
+                names=['scorer', 'bodyparts', 'coords']
+            )
+            df = pd.DataFrame(
+                index=file_names,
+                columns=header,
+            )
+
+            # populate the dataframe
+            for label, coord in zip(points_layer.properties['label'], points_layer.data):
+                fname = all_files[coord[0].astype(np.int)]
+                df.loc[fname][scorer][label]['x'] = coord[2]
+                df.loc[fname][scorer][label]['y'] = coord[1]
+
+            # write the dataframe
+            df.to_csv(output_path)
 
         # connect the events and add the label menu widget
         points_layer.events.current_properties.connect(update_label_menu)
