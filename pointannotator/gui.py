78146d958a626c5442b06ccbfee3a336b80d1021
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-06-01 15:23:27 +0200

    reduce nested functions

--- pointannotator/gui.py
+++ pointannotator/gui.py
@@ -1,11 +1,9 @@
-import glob
 from typing import List
 
 from dask_image.imread import imread
 import napari
 import numpy as np
 from magicgui import magicgui
-import pandas as pd
 
 COLOR_CYCLE = [
         '#1f77b4',
@@ -21,11 +19,49 @@ COLOR_CYCLE = [
 ]
 
 
+def create_label_menu(points_layer, labels):
+    """Create a label menu widget that can be added to the napari viewer dock
+
+    Parameters:
+    -----------
+    points_layer : napari.layers.Points
+        a napari points layer
+    labels : List[str]
+        list of the labels for each keypoint to be annotated (e.g., the body parts to be labeled).
+
+    Returns:
+    --------
+    label_menu : QComboBox
+        the label menu qt widget
+    """
+    # Create the label selection menu
+    @magicgui(label={'choices': labels})
+    def label_selection(label):
+        return label
+
+    label_menu = label_selection.Gui()
+
+    def update_label_menu(event):
+        """Update the label menu when the point selection changes"""
+        label_menu.label = points_layer.current_properties['label'][0]
+
+    points_layer.events.current_properties.connect(update_label_menu)
+
+    def label_changed(result):
+        """Update the Points layer when the label menu selection changes"""
+        selected_label = result
+        current_properties = points_layer.current_properties
+        current_properties['label'] = np.asarray([selected_label])
+        points_layer.current_properties = current_properties
+
+    label_menu.label_changed.connect(label_changed)
+
+    return label_menu
+
+
 def point_annotator(
         im_path: str,
         labels: List[str],
-        output_path: str='anno.csv',
-        scorer: str='user'
 ):
     """Create a GUI for annotating points in a series of images.
 
@@ -35,17 +71,12 @@ def point_annotator(
         glob-like string for the images to be labeled.
     labels : List[str]
         list of the labels for each keypoint to be annotated (e.g., the body parts to be labeled).
-    output_path : str
-        path to the csv file to which the annotations will be saved
-    scorer : str
-        name of the person performing the annotation
     """
     stack = imread(im_path)
     with napari.gui_qt():
-        viewer = napari.view_image(stack, contrast_limits=[0, 255])
-        properties = {'label': labels}
+        viewer = napari.view_image(stack)
         points_layer = viewer.add_points(
-            properties=properties,
+            properties={'label': labels},
             edge_color='label',
             edge_color_cycle=COLOR_CYCLE,
             symbol='o',
@@ -55,28 +86,8 @@ def point_annotator(
         )
         points_layer.edge_color_mode = 'cycle'
 
-        # Create the label selection menu
-        @magicgui(label={'choices': labels})
-        def label_selection(label):
-            return label
-        label_menu = label_selection.Gui()
-
-        def update_label_menu(event):
-            """Update the label menu when the point selection changes"""
-            label_menu.label = points_layer.current_properties['label'][0]
-
-        points_layer.events.current_properties.connect(update_label_menu)
-
-        def label_changed(result):
-            """Update the Points layer when the label menu selection changes"""
-            selected_label = result
-            current_properties = points_layer.current_properties
-            current_properties['label'] = np.asarray([selected_label])
-            points_layer.current_properties = current_properties
-
-        label_menu.label_changed.connect(label_changed)
-
         # add the label menu widget to the viewer
+        label_menu = create_label_menu(points_layer, labels)
         viewer.window.add_dock_widget(label_menu)
 
         @viewer.bind_key('.')
@@ -102,7 +113,6 @@ def point_annotator(
         points_layer.mode = 'add'
         points_layer.mouse_drag_callbacks.append(next_on_click)
 
-
         @viewer.bind_key(',')
         def prev_label(event):
             """Keybinding to decrement to the previous label with wraparound"""
@@ -114,9 +124,3 @@ def point_annotator(
             new_label = labels[new_ind]
             current_properties['label'] = np.array([new_label])
             points_layer.current_properties = current_properties
-
-
-        @viewer.bind_key('Control-Alt-S')
-        def save_points(event):
-            """Save the added points to a CSV file"""
-            points_layer.save(output_path)
